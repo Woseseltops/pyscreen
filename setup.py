4fee2bc8b1fb3e92abaa22fedd9552bb199214e8
Wessel Stoop <wstoop@davincigroep.nl>
2015-12-01 10:14:05 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 setup(
   name = 'pyscreen',
   packages = ['pyscreen'],
-  version = '0.1',
+  version = '0.3',
   description = 'A light-weight library for easy interaction between Python and GNU screen.',
-  long_description = open('README.md').read(),
   author = 'Wessel Stoop',
   author_email = 'stoopwessel@gmail.com',
   url = 'https://github.com/woseseltops/pyscreen',
-  download_url = 'https://github.com/woseseltops/pyscreen/tarball/0.1',
+  download_url = 'https://github.com/woseseltops/pyscreen/tarball/0.3',
   keywords = ['GNU screen', 'screen'],
   classifiers = [],
 )
