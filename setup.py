d63d798f9b5848303225acfe22cc01e236b86441
Wessel Stoop <wstoop@davincigroep.nl>
2015-12-01 09:28:28 +0100

    Added some stuff for a pypi release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyscreen',
+  packages = ['pyscreen'],
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Wessel Stoop',
+  author_email = 'stoopwessel@gmail.com',
+  url = 'https://github.com/woseseltops/pyscreen',
+  download_url = 'https://github.com/woseseltops/pyscreen/tarball/0.1',
+  keywords = ['GNU screen', 'screen'],
+  classifiers = [],
+)
